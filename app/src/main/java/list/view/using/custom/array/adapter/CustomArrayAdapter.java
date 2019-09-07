c88f75e99ebac3c4c814737debb9c3b49e100ff9
rohitnotes <rohitnotes24@gmail.com>
2019-09-07 14:28:54 +0530

    Initial commit

---
+++ app/src/main/java/list/view/using/custom/array/adapter/CustomArrayAdapter.java
@@ -0,0 +1,66 @@
+package list.view.using.custom.array.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class CustomArrayAdapter extends ArrayAdapter<ItemModel> {
+
+    private LayoutInflater layoutInflater;
+
+    public CustomArrayAdapter(Context context, ArrayList<ItemModel> itemModelArrayList) {
+        super(context, 0 , itemModelArrayList);
+        layoutInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ItemViewHolder itemViewHolder;
+
+        if (convertView == null)
+        {
+            convertView = layoutInflater.inflate(R.layout.list_item, parent, false);
+              /*
+            OR
+            convertView = layoutInflater.inflate(R.layout.layout_list_item,null);
+             */
+            itemViewHolder = new ItemViewHolder(convertView);
+            convertView.setTag(itemViewHolder);
+        }
+        else
+        {
+            itemViewHolder = (ItemViewHolder) convertView.getTag();
+        }
+
+        ItemModel rowItem = (ItemModel) getItem(position);
+        ((ItemViewHolder)itemViewHolder).setData(rowItem);
+        return convertView;
+    }
+
+    private class ItemViewHolder
+    {
+        TextView tvTitle, tvDesc;
+        ImageView ivIcon;
+
+        public ItemViewHolder(View item)
+        {
+            tvTitle = (TextView) item.findViewById(R.id.tvTitle);
+            tvDesc  = (TextView) item.findViewById(R.id.tvDesc);
+            ivIcon  = (ImageView) item.findViewById(R.id.ivIcon);
+        }
+
+        public void setData(ItemModel itemPosition)
+        {
+            tvTitle.setText(itemPosition.getTitle());
+            tvDesc.setText(itemPosition.getDescription());
+            ivIcon.setImageResource(itemPosition.getImgResId());
+        }
+    }
+}
